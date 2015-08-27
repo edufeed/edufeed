2905c00ac17726a5abeb3158a7ad5ef5052e1152
Geza Kovacs <geza@cs.stanford.edu>
2015-08-27 11:30:29 -0700

    added missing files

---
+++ www/iframe-activity-example.js
@@ -0,0 +1,43 @@
+(function(){
+  var getUrlParameters, sendmessage, addlog, sendSampleLogs, leaveActivity, finishActivity, out$ = typeof exports != 'undefined' && exports || this;
+  out$.getUrlParameters = getUrlParameters = function(){
+    var url, hash, map, parts;
+    url = window.location.href;
+    hash = url.lastIndexOf('#');
+    if (hash !== -1) {
+      url = url.slice(0, hash);
+    }
+    map = {};
+    parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value){
+      return map[key] = decodeURIComponent(value).split('+').join(' ');
+    });
+    return map;
+  };
+  out$.sendmessage = sendmessage = function(messagetype, data){
+    return parent.postMessage({
+      messagetype: messagetype,
+      data: data
+    }, '*');
+  };
+  out$.addlog = addlog = function(data){
+    return sendmessage('addlog', data);
+  };
+  out$.sendSampleLogs = sendSampleLogs = function(){
+    return addlog({
+      somemessage: 'barbar',
+      bax: 'qux'
+    });
+  };
+  out$.leaveActivity = leaveActivity = function(){
+    return sendmessage('task-left');
+  };
+  out$.finishActivity = finishActivity = function(){
+    return sendmessage('task-finished');
+  };
+  $(document).ready(function(){
+    var params;
+    params = getUrlParameters();
+    $('#foodiv').text(params.foo);
+    return $('#bardiv').text(params.bar);
+  });
+}).call(this);
