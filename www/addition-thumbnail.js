7603f957c3c6b747df894d7d9adac5c575b0d9a7
unknown <t-kileys@T-KILEYS-DT.fareast.corp.microsoft.com>
2015-09-10 13:02:00 +0530

    Changed how we create thumbnails for dots, balance, and addition.

--- www/addition-thumbnail.js
+++ www/addition-thumbnail.js
@@ -2,14 +2,6 @@
   RegisterThumbnail({
     is: 'addition-thumbnail',
     properties: {
-      thumbnailpage: {
-        type: String,
-        value: 'addition.html'
-      },
-      thumbnailurl: {
-        type: String,
-        computed: 'compute_thumbnailurl(thumbnailpage, sum, add)'
-      },
       sum: {
         type: Number,
         value: 2
@@ -19,18 +11,5 @@
         value: 1
       }
     },
-    compute_thumbnailurl: function(thumbnailpage, sum, add){
-      var separator, nparams;
-      separator = '?';
-      if (thumbnailpage.indexOf('?') !== -1) {
-        separator = '&';
-      }
-      nparams = {
-        isthumbnail: true,
-        sum: sum,
-        add: add
-      };
-      return thumbnailpage + separator + $.param(nparams);
-    }
   });
 }).call(this);
