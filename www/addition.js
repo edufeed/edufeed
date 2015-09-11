a78e2f63e45d5617cd3bfb96e2c7738a28832372
unknown <t-kileys@T-KILEYS-DT.fareast.corp.microsoft.com>
2015-09-11 15:31:41 +0530

    Changed some UI for addition.

--- www/addition.js
+++ www/addition.js
@@ -46,12 +46,12 @@
             }
             else {
                 //document.getElementById("boink").play();
-                play_sound('audio/boink.mp3', function () {
+                /*play_sound('audio/boink.mp3', function () {
                     //finishActivity();
-                });
+                });*/
             }
             d3.select(this)
-                .attr("x", 20)
+                .attr("x", 30)
                 .attr("y", parseInt(d3.select(this).attr("data-y"), 10));
 
             mousedragX = 0;
@@ -81,45 +81,47 @@ function InitProblem() {
         .attr("id", "addend1number")
         .attr("x", 335)
         .attr("y", 100)
+        .attr("font-family", 'Arial Rounded MT')
+        .attr("font-size", "30px")
         .text(addend1);
 
     problem_g.append("text")
+        .attr("font-size", "30px")
+        .attr("font-family", "Arial Rounded MT")
         .attr("id", "plussign")
-        .attr("x", 320)
+        .attr("x", 310)
         .attr("y", 130)
         .text("+");
 
     problem_g.append("text")
+        .attr("font-size", "30px")
+        .attr("font-family", "Arial Rounded MT")
         .attr("id", "addend2number")
         .attr("x", 335)
         .attr("y", 130)
-        .text("?");
+        .text("⬜");
 
     problem_g.append("line")
         .attr("x1", 310)
         .attr("x2", 350)
-        .attr("y1", 150)
-        .attr("y2", 150)
+        .attr("y1", 140)
+        .attr("y2", 140)
         .attr("stroke-width", 1)
         .attr("stroke", "black");
 
     problem_g.append("text")
-        .attr("id", "equalsign")
-        .attr("x", 320)
-        .attr("y", 180)
-        .text("=");
-
-    problem_g.append("text")
+        .attr("font-size", "30px")
+        .attr("font-family", "Arial Rounded MT")
         .attr("id", "sumnumber")
         .attr("x", 335)
-        .attr("y", 180)
+        .attr("y", 170)
         .text(sum);
 
     additionbar_g.append("rect")
         .attr("id", "addend1bar")
         .attr("class", "addend1bar")
         .attr("height", 10)
-        .attr("x", 100)
+        .attr("x", 150)
         .attr("y", 110)
         .attr("data-value", addend1)
         .attr("width", addend1 * 10);
@@ -128,7 +130,7 @@ function InitProblem() {
         .attr("id", "sumbar")
         .attr("class", "sumbar")
         .attr("height", 10)
-        .attr("x", 100)
+        .attr("x", 150)
         .attr("y", 170)
         .attr("data-value", sum)
         .attr("width", sum * 10);
